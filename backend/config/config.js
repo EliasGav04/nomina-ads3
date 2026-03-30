ad659ca854f2d7a0b7fabb2ace61eda516b17d5b
RenanOyuelaJr <1230546@usap.edu>
2026-03-30 00:16:53 +0000

    Updates to finish with migrations

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,25 +1,27 @@
 require('dotenv').config();
 
+const dialect = process.env.DB_DIALECT || 'mysql'; // 
+
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || 'C0zumel2025!',
+    database: process.env.DB_NAME || 'nomina',
+    host: process.env.DB_HOST || 'localhost',
+    dialect, // 
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME + '_test',
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || null,
+    database: (process.env.DB_NAME || 'nomina') + '_test',
+    host: process.env.DB_HOST || 'localhost',
+    dialect,
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || null,
+    database: process.env.DB_NAME || 'nomina',
+    host: process.env.DB_HOST || 'localhost',
+    dialect,
   }
 };
